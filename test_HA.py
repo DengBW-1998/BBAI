b995bfdff606b3ee5906472b0245cc9ea2ccea46
DengBW <61540016+DengBW-1998@users.noreply.github.com>
2024-01-25 15:55:02 +0800

    Update test_HA.py

--- test_HA.py
+++ test_HA.py
@@ -16,39 +16,45 @@ import deeprobust.graph.defense as dr
 from sklearn.model_selection import train_test_split
 from BGNN.bgnn_adv import *
 from BGNN.bgnn_mlp import *
+from memory_profiler import profile
 
-n_flips = -1 #Perturbation number
-dim = 64 
-window_size = 5 
-n_node_pairs = 100000 
-seed=2
+n_flips = -1 #Number of perturbations per iteration
+dim = 64 #Embedding vector dimension
+window_size = 5 #Window size
+n_node_pairs = 100000 #Number of test edges
 threshold = 5 #Implicit relationship threshold
-dataset='dblp'
-rate=-1
-train_model='netmf'
 batch_size=64
-data=open("HA_dblp.txt",'w+')
+dataset='wiki'
+rate=1
+train_model='netmf'
+ptb_rate=10#rate of perturbed edges
+file_name="HA_"+dataset+"_"+str(ptb_rate)+"_"+train_model+".txt"
+data_file=open(file_name, 'w+')
 
 if dataset=='dblp':
-    n_flips=90
+    n_flips=int(1800*ptb_rate/100)
     rate=5
 if dataset=='wiki':
-    n_flips=180
+    n_flips=int(3600*ptb_rate/100)
     rate=10
-if dataset=='pubmed':
-    n_flips=70
-  
-    
+if dataset == 'citeseer':
+    n_flips = int(2840 / 2 * ptb_rate / 100)
+if dataset == 'pubmed':
+    n_flips = int(38782 / 2 * ptb_rate / 100)
+n_candidates=10*n_flips #Number of candidate perturbed edges
+
+seed=2
 np.random.seed(seed)
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 
-adj_nn,adj,u,v = getAdj(threshold,dataset,rate)
+adj_nn,adj,u,v,test_labels = getAdj(threshold,dataset,rate)
 emb0_u,emb0_v,dim_u,dim_v = getAttribut(u,v,dataset)  
 adj = standardize(adj)
 
-labels = np.zeros((u+v))
-labels[u:] = 1
+
+vlabels = pd.read_csv('./data/' + dataset + '_' + 'vlabels.dat', sep=' ', header=None).to_numpy().astype(int)
+vlabels = np.squeeze(vlabels)[:u+v]
 
 def get_train_val_test(idx, train_size, val_size, test_size, stratify):
 
@@ -92,7 +98,7 @@ test_size = 0.8
 train_size = 1 - test_size - val_size
 
 idx = np.arange(adj.shape[0])
-idx_train, idx_val, idx_test = get_train_val_test(idx, train_size, val_size, test_size, stratify=labels)
+idx_train, idx_val, idx_test = get_train_val_test(idx, train_size, val_size, test_size, stratify=vlabels)
 idx_unlabeled = np.union1d(idx_val, idx_test)
 
 features = np.ones((adj.shape[0],32))
@@ -113,7 +119,7 @@ def compute_lambda(adj, idx_train, idx_test):
     train_test = num_all - train_train - test_test
     return train_train / num_all, train_test / num_all, test_test / num_all
 
-
+@profile
 def heuristic_attack(adj, n_perturbations, idx_train, idx_unlabeled, lambda_1, lambda_2):
     idx_train = torch.LongTensor(idx_train)
     idx_test = torch.LongTensor(idx_unlabeled)
@@ -129,7 +135,7 @@ def heuristic_attack(adj, n_perturbations, idx_train, idx_unlabeled, lambda_1, l
     while cnt < n_train:
         node_1 = np.random.choice(canditate_train_idx, 1)
         node_2 = np.random.choice(canditate_train_idx, 1)
-        if labels[node_1] != labels[node_2] and adj[node_1, node_2] == 0:
+        if vlabels[node_1] != vlabels[node_2] and adj[node_1, node_2] == 0:
             perturbed_adj[node_1, node_2] = 1
             perturbed_adj[node_2, node_1] = 1
             cnt += 1
@@ -138,7 +144,7 @@ def heuristic_attack(adj, n_perturbations, idx_train, idx_unlabeled, lambda_1, l
     while cnt < n_test:
         node_1 = np.random.choice(canditate_train_idx, 1)
         node_2 = np.random.choice(candidate_test_idx, 1)
-        if labels[node_1] != labels[node_2] and perturbed_adj[node_1, node_2] == 0:
+        if vlabels[node_1] != vlabels[node_2] and perturbed_adj[node_1, node_2] == 0:
             perturbed_adj[node_1, node_2] = 1
             perturbed_adj[node_2, node_1] = 1
             cnt += 1
@@ -152,7 +158,7 @@ adj_matrix_flipped = heuristic_attack(torch.from_numpy(adj).float(), n_flips, id
 time_end=time.time()
 adj_matrix_flipped = sp.csr_matrix(adj_matrix_flipped)
 
-for _ in range(1): 
+for _ in range(5):
     u_node_pairs = np.random.randint(0, u-1, [n_node_pairs*2, 1])
     v_node_pairs = np.random.randint(u, u+v-1, [n_node_pairs*2, 1])
     node_pairs = np.column_stack((u_node_pairs,v_node_pairs))
@@ -169,23 +175,27 @@ for _ in range(1):
     if train_model=='bgnn':
         bgnn = BGNNAdversarial(u,v,batch_size,adj_matrix_flipped[:u,u:],adj_matrix_flipped[u:,:u],emb0_u,emb0_v, dim_u,dim_v, dataset)
         embedding = bgnn.adversarial_learning()
-        
-    auc_score = evaluate_embedding_link_prediction(
-        adj_matrix=adj_matrix_flipped, 
-        node_pairs=node_pairs, 
-        embedding_matrix=embedding
-    )
-    print('svd HA auc:{}'.format(auc_score))
-    print('svd HA auc:{}'.format(auc_score),file=data)
+    if dataset == 'dblp' or dataset == 'wiki':
+        auc_score = evaluate_embedding_link_prediction(
+            adj_matrix=adj_matrix_flipped,
+            node_pairs=node_pairs,
+            embedding_matrix=embedding
+        )
+        print('HA auc:{:.5f}'.format(auc_score))
+        print('HA auc:{:.5f}'.format(auc_score), file=data_file)
+    else:
+        f1_scores_mean, _ = evaluate_embedding_node_classification(embedding, test_labels)
+        print('HA, F1: {:.5f} {:.5f}'.format(f1_scores_mean[0], f1_scores_mean[1]))
+        print('HA, F1: {:.5f} {:.5f}'.format(f1_scores_mean[0], f1_scores_mean[1]), file=data_file)
 
 print(train_model)
-print(train_model,file=data)
+print(train_model, file=data_file)
 print('HA') 
-print('HA',file=data)
+print('HA', file=data_file)
 print(dataset)
-print(dataset,file=data) 
+print(dataset, file=data_file)
 print(time_end-time_start)
-print(time_end-time_start,file=data)     
-data.close()
+print(time_end - time_start, file=data_file)
+data_file.close()
 
 
